e9bec5f47425b30aedfba8c51f107dc3bb9eacc9
Kasper Sundström <kasper.sundstrom@helsinki.fi>
2022-01-26 23:24:12 +0200

    Updates to beautifulsoup_test.py
    Structured with main function, changed to English version of weather report, and simplified some code

--- beautifulsoup_test.py
+++ beautifulsoup_test.py
@@ -1,21 +1,25 @@
+#!/usr/bin/env python3
+
+
 from urllib import request
 from bs4 import BeautifulSoup
 
 
-# Access FMI's website and download the raw site data of the weather report for sailors
-url = "https://www.ilmatieteenlaitos.fi/saatiedotus-merenkulkijoille"
-response = request.urlopen(url)
-html_doc = response.read().decode('utf8')
+def main():
+    # Access FMI's website and download the raw site data of the weather forecast for shipping in English
+    url = "https://en.ilmatieteenlaitos.fi/weather-forecast-for-shipping"
+    html = request.urlopen(url).read().decode('utf8')
 
-# Run the raw HTML through BeautifulSoup
-soup = BeautifulSoup(html_doc, 'html.parser')
+    # Run the raw HTML through BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
 
-# Access the main body of the weather report through css-classes
+    # Access the main body of the weather report through css-classes
+    #texts = soup.find_all('p')
+    #for text in texts:
+    #    print(text.get_text())
+    forecast = soup.find(id = 'proxyId_1NG1s0PovRLtQcAthgOmNp')
+    print(forecast.get_text())
 
-#texts = soup.find_all('p')
-#for text in texts:
-#    print(text.get_text())
 
-forecast = soup.find(id = 'proxyId_2eI9z8S0RBAYRYtyyE9qu8')
-for text in forecast:
-    print(text.get_text())
+if __name__ == '__main__':
+    main()
