da6005168dbbde3eedca36a8fb4cc12b73c439c9
Juho Kajava <juho@kajava.co>
2022-02-02 23:10:24 +0200

    #6, #7, The script can now index and search through Shakespeare's sonnets and present the findings in plain text. Added this also to the issue #7, since this _might_ be considered 'real world data' but alas, I now need to sleep.

--- searchengine.py
+++ searchengine.py
@@ -4,7 +4,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from bs4 import BeautifulSoup as bs
 from urllib import request
-
+import re
 
 
  
@@ -21,10 +21,17 @@ DOCUMENTS = ["This is a silly example",
              "That was the real catch-22. The year 1923."]
 
 # Read the database file
-content = []
 url = "https://www.gutenberg.org/cache/epub/1041/pg1041.txt"
 html = request.urlopen(url).read().decode('utf8')
 
+newlines_removed = html.replace("\r\n", "#")
+
+pattern = '((?#*)[IVXLC]+#+)'
+sonnets_hash = re.split(pattern, newlines_removed)
+sonnets = []
+for sonnet in sonnets_hash:
+    sonnet = sonnet.replace("#", "")
+    sonnets.append(sonnet)                            
 
 def get_query():
     while True:
@@ -46,7 +53,7 @@ def rewrite_query(query):
 
 if __name__ == '__main__':
         cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'(?u)\b\w\w*\b') # The pattern now accepts tokens of lenght 1 such as "a"
-        sparse_matrix = cv.fit_transform(DOCUMENTS)
+        sparse_matrix = cv.fit_transform(sonnets)
         sparse_td_matrix = sparse_matrix.T.tocsr()
         t2i = cv.vocabulary_
         while True:
@@ -56,7 +63,7 @@ if __name__ == '__main__':
                     hits_matrix = eval(rewrite_query(query))
                     hits_list = list(hits_matrix.nonzero()[1])
                     for i, doc_idx in enumerate(hits_list):
-                        print("Matching doc #{:d}: {:s}".format(i, DOCUMENTS[doc_idx]))
+                        print("Matching doc #{:d}: {:s}".format(i, sonnets[doc_idx]))
                 else:
                     break
             except KeyError:
