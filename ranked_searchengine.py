1614c0e0b1d207fb037cc9a7a39c97ecbde1f7ab
Juho Kajava <juho@kajava.co>
2022-02-09 21:48:19 +0200

    Made changes to ranked_searchengine.py. There is a problem with the countvectorizer.

--- ranked_searchengine.py
+++ ranked_searchengine.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from bs4 import BeautifulSoup
 from urllib import request
 import re
+import numpy as np
 
 
 D = {"and": "&", "AND": "&",
@@ -14,20 +16,10 @@ D = {"and": "&", "AND": "&",
 
 ARTICLES_FILENAME = "fiwiki-20181001-corpus.truncated.txt"
 
-SONNETS_URL = "https://www.gutenberg.org/cache/epub/1041/pg1041.txt"
 
 
-def get_source():
-    print()
-    while True:
-        user_input = input("Please type ‘a’ to use articles or ‘s’ to use sonnets as documents: ")
-        if user_input == "a":
-            print("Using articles as documents…")
-            return "a"
-        elif user_input == "s":
-            print("Using sonnets as documents…")
-            return "s"
-
+tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+cv = CountVectorizer(lowercase=True)
 
 def get_articles():
     try:
@@ -40,55 +32,33 @@ def get_articles():
     except OSError:
         print("Error trying to read from file {:s}!".format(ARTICLES_FILENAME))
 
-
-def get_sonnets():
-    html = request.urlopen(SONNETS_URL).read().decode('utf8')
-    newlines_removed = html.replace("\r\n", "#")
-    pattern = '((?#*)[IVXLC]+#+)'
-    sonnets_hash = re.split(pattern, newlines_removed)
-    sonnets = []
-    for sonnet in sonnets_hash:
-        sonnet = sonnet.replace("#", "")
-        sonnets.append(sonnet)
-    return sonnets
-
-
 def get_query():
     print()
     return input("Please enter a query or press enter to exit: ").strip()
 
 
-def rewrite_token(t):
-    return D.get(t, 'sparse_td_matrix[cv.vocabulary_["{:s}"]].todense()'.format(t))
+def get_hits(query):
+    query_as_words = query.split(" ")
+    hit_list = []
+    for word in query_as_words:
+        hit_list = np.array.sum(hit_list, tf_matrix[t2i[query]])
+    hits_and_doc_ids = [ (hits, i) for i, hits in enumerate(hit_list) if hits > 0 ]
+    ranked_hits_and_doc_ids = sorted(hits_and_doc_ids, reverse=True)
+    return ranked_hits_and_doc_ids
 
-
-def rewrite_query(query):
-    return " ".join(rewrite_token(t) for t in query.split())
-
-
-def main():
-    cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'(?u)\b\w\w*\b')
-    source = get_source()
-    if source == "a":
-        documents = get_articles()
-    elif source == "s":
-        documents = get_sonnets()
-    sparse_matrix = cv.fit_transform(documents)
-    sparse_td_matrix = sparse_matrix.T.tocsr()
+if __name__ == '__main__':
+    t2i = cv.vocabulary_
+    articles = get_articles()
+    tf_matrix = tfv.fit_transform(articles).T.todense()
     while True:
         try:
             query = get_query()
             if query:
-                hits_matrix = eval(rewrite_query(query))
-                hits_list = list(hits_matrix.nonzero()[1])
-                for i, doc_idx in enumerate(hits_list):
-                    print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
-                    print()
+                article_hits = get_hits(query)
+                for hit, i in article_hits:
+                    print(f"Score of '{query}' is {hit}")
             else:
                 break
         except KeyError:
             print("Your search provided no results, please try another query.")
-
-
-if __name__ == '__main__':
-    main()
+    
