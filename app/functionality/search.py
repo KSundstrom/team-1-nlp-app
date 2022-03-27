f3557632025b73147867cfd84ad74d3ffb0ffdea
Kasper Sundström <kasper.sundstrom@helsinki.fi>
2022-03-27 22:32:33 +0300

    HTML corrections for weather data

--- app/functionality/search.py
+++ app/functionality/search.py
@@ -10,5 +10,5 @@ def search(vectorizer, data_dictionaries, data_matrix, query):
     hits = np.dot(query_vector, data_matrix)
     ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse = True)
     for i, (score, id) in enumerate(ranked_scores_and_doc_ids):
-        matches.append({'hit':"{:d}".format(i+1), 'score':"{:.4f}".format(score), 'date':"{:s}".format(data_dictionaries[id]['date']), 'title':"{:s}".format(data_dictionaries[id]['title']), 'place':"{:s}…".format(data_dictionaries[id]['place']), 'forecast':"{:s}…".format(data_dictionaries[id]['forecast']), 'forecast text':"{:s}".format(data_dictionaries[id]['forecast text'])})
+        matches.append({'hit':"{:d}".format(i+1), 'score':"{:.4f}".format(score), 'date':"{:s}".format(data_dictionaries[id]['date']), 'title':"{:s}".format(data_dictionaries[id]['title']), 'place':"{:s}".format(data_dictionaries[id]['place']), 'forecast':"{:s}".format(data_dictionaries[id]['forecast']), 'forecast text':"{:s}".format(data_dictionaries[id]['forecast text'])})
     return matches
