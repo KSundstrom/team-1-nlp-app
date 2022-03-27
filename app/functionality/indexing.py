03d0d1583f2cbc52697d7e2f7c330e50ecf8b3eb
Kasper Sundström <kasper.sundstrom@helsinki.fi>
2022-03-27 22:43:06 +0300

    Include all contents in vectorizer

--- app/functionality/indexing.py
+++ app/functionality/indexing.py
@@ -7,6 +7,6 @@ import json
 def index(vectorizer, sourcepath):
     with open(sourcepath) as file:
         data_dictionaries = json.load(file)
-    documents = [d['forecast text'] for d in data_dictionaries if 'forecast text' in d]
-    data_matrix = vectorizer.fit_transform(documents).T.tocsr()
+    data_list = [' '.join([d['date'], d['title'], d['place'], d['forecast'], d['forecast text']]) for d in data_dictionaries]
+    data_matrix = vectorizer.fit_transform(data_list).T.tocsr()
     return data_dictionaries, data_matrix
