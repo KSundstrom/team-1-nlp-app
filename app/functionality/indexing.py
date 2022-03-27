933acf1d3172f77e5cbd5b4061a4ea21fa81e5b3
Kasper Sundström <kasper.sundstrom@helsinki.fi>
2022-03-27 22:26:40 +0300

    Rewrite functions to work with the JSON weather data

--- app/functionality/indexing.py
+++ app/functionality/indexing.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 
-from bs4 import BeautifulSoup as bs
+import json
 
 
 def index(vectorizer, sourcepath):
-    document_dictionaries = []
     with open(sourcepath) as file:
-        soup = bs(file, 'lxml')
-    for article in soup.find_all('article'):
-        document_dictionaries.append({'name':article['name'], 'content':article.get_text(strip=True)})
-    documents = [d['content'] for d in document_dictionaries if 'content' in d]
-    document_matrix = vectorizer.fit_transform(documents).T.tocsr()
-    return document_dictionaries, document_matrix
+        data_dictionaries = json.load(file)
+    documents = [d['forecast text'] for d in data_dictionaries if 'forecast text' in d]
+    data_matrix = vectorizer.fit_transform(documents).T.tocsr()
+    return data_dictionaries, data_matrix
--- app/functionality/search.py
+++ app/functionality/search.py
@@ -4,11 +4,11 @@
 import numpy as np
 
 
-def search(vectorizer, document_dictionaries, document_matrix, query):
+def search(vectorizer, data_dictionaries, data_matrix, query):
     matches = []
     query_vector = vectorizer.transform([query]).tocsc()
-    hits = np.dot(query_vector, document_matrix)
+    hits = np.dot(query_vector, data_matrix)
     ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse = True)
     for i, (score, id) in enumerate(ranked_scores_and_doc_ids):
-        matches.append({'hit':"{:d}".format(i+1), 'score':"{:.4f}".format(score), 'name':"{:s}".format(document_dictionaries[id]['name']), 'content':"{:.100s}…".format(document_dictionaries[id]['content'])})
+        matches.append({'hit':"{:d}".format(i+1), 'score':"{:.4f}".format(score), 'date':"{:s}".format(data_dictionaries[id]['date']), 'title':"{:s}".format(data_dictionaries[id]['title']), 'place':"{:s}…".format(data_dictionaries[id]['place']), 'forecast':"{:s}…".format(data_dictionaries[id]['forecast']), 'forecast text':"{:s}".format(data_dictionaries[id]['forecast text'])})
     return matches
